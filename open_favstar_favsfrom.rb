2c8320bbe88f4fb992ed9d88ef665d086420024d
cosmo(コスモ) <cosmo_originstring@yahoo.co.jp>
2011-09-24 01:03:14 +0900

    mikutterコマンド追加プラグインfavstar:favs_from追加

---
+++ open_favstar_favsfrom.rb
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# favstarを開く
+
+Module.new do
+  Plugin.create(:open_favstar_favs_from).add_event_filter(:command){ |menu|
+    menu[:open_favstar_favs_from] = {
+      :slug => :open_favstar_favs_from,
+      :name => 'この人のfavstar:favs_fromを見る',
+      :condition => lambda{ |m| m.message.repliable? },
+      :exec => lambda{ |m| Gtk::openurl("http://favstar.fm/users/#{m.message.user.idname}/favs_from") },
+      :visible => true,
+      :role => :message }
+    [menu]
+  }
+end
