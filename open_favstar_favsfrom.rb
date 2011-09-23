f88e4947feb719e42af7c3786f911869d2555780
cosmo(コスモ) <cosmo_originstring@yahoo.co.jp>
2011-09-24 01:08:44 +0900

    favstar/favs_fromを修正

--- open_favstar_favsfrom.rb
+++ open_favstar_favsfrom.rb
@@ -5,7 +5,7 @@ Module.new do
   Plugin.create(:open_favstar_favs_from).add_event_filter(:command){ |menu|
     menu[:open_favstar_favs_from] = {
       :slug => :open_favstar_favs_from,
-      :name => 'この人のfavstar:favs_fromを見る',
+      :name => 'この人のfavstar:favsfromを見る',
       :condition => lambda{ |m| m.message.repliable? },
       :exec => lambda{ |m| Gtk::openurl("http://favstar.fm/users/#{m.message.user.idname}/favs_from") },
       :visible => true,
