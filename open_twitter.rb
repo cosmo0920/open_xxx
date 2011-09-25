c233f0a7ecbb8047d199ba633125de2e5e2f11c5
cosmo(コスモ) <cosmo_originstring@yahoo.co.jp>
2011-09-25 18:05:23 +0900

    ショートカットキー作成時のmikutterへの干渉によりクラッシュする問題を修正

--- open_twitter.rb
+++ open_twitter.rb
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
-# favstarを開く
+# twitter Webを開く
 
 Module.new do
   Plugin.create(:open_twitter).add_event_filter(:command){ |menu|
     menu[:open_twitter] = {
       :slug => :open_twitter,
-      :name => lambda{ |m| "#{m.message.user.idname.gsub('_','__')}さんのtwitter Webへ"},
+      :name => 'この人のtwitterを見る', 
+      :show_face => lambda{ |m|
+          u = if(m.message[:retweet])
+                m.message[:retweet].user
+              else
+                m.message.user end
+          "#{u[:idname]}さんのtwitter Webへ".gsub(/_/, '__') },
       :condition => lambda{ |m| m.message.repliable? },
       :exec => lambda{ |m| Gtk::openurl("http://twitter.com/#!/#{m.message.user.idname}") },
       :visible => true,
