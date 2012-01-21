80d0b86e24de3cf556b5c1e6a5321cf306f58261
cosmo(コスモ) <cosmo_originstring@yahoo.co.jp>
2012-01-21 09:31:11 +0900

    Twilogを開くコマンドを追加するプラグイン

---
+++ open_twilog.rb
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# twitter Webを開く
+
+Module.new do
+  Plugin.create(:open_twilog).add_event_filter(:command){ |menu|
+    menu[:open_twilog] = {
+      :slug => :open_twilog,
+      :name => 'この人のtwilogを見る', 
+      :show_face => lambda{ |m|
+          u = if(m.message[:retweet])
+                m.message[:retweet].user
+              else
+                m.message.user end
+          "#{u[:idname]}さんのtwilogへ".gsub(/_/, '__') },
+      :condition => lambda{ |m| m.message.repliable? },
+      :exec => lambda{ |m| Gtk::openurl("http://twilog.org/#{m.message.user.idname}") },
+      :visible => true,
+      :role => :message }
+    [menu]
+  }
+end
