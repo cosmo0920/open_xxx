ebdef63c681ca6e51a88caacce1c4e64acdd1d7f
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2012-10-02 00:35:50 +0900

    open_twilogを0.2対応に

--- open_twilog.rb
+++ open_twilog.rb
@@ -1,21 +1,16 @@
 # -*- coding: utf-8 -*-
-# twilogを開く
-
-Module.new do
-  Plugin.create(:open_twilog).add_event_filter(:command){ |menu|
-    menu[:open_twilog] = {
-      :slug => :open_twilog,
-      :name => 'この人のtwilogを見る', 
-      :show_face => lambda{ |m|
-          u = if(m.message[:retweet])
-                m.message[:retweet].user
+# twitter Webを開く
+Plugin.create :open_twilog do
+    command(:open_twilog,
+      name: 'この人のtwilogを見る',
+      condition: Proc.new{ |m|
+          u = if(m.messages.first.message[:retweet])
+                m.messages.first.message[:retweet].user
               else
-                m.message.user end
+                m.messages.first.message.user end
           "#{u[:idname]}さんのtwilogへ".gsub(/_/, '__') },
-      :condition => lambda{ |m| m.message.repliable? },
-      :exec => lambda{ |m| Gtk::openurl("http://twilog.org/#{m.message.user.idname}") },
-      :visible => true,
-      :role => :message }
-    [menu]
-  }
+      visible: true,
+      role: :timeline) do |opt|
+        Gtk::openurl("http://twilog.org/#{opt.messages.first.message.idname}")
+  end
 end
