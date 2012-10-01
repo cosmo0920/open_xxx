b74002729d34b59a0fb5af51a5a7256f2a5014c0
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2012-10-02 00:35:05 +0900

    open_twiterを0.2対応に

--- open_twitter.rb
+++ open_twitter.rb
@@ -1,21 +1,17 @@
 # -*- coding: utf-8 -*-
 # twitter Webを開く
 
-Module.new do
-  Plugin.create(:open_twitter).add_event_filter(:command){ |menu|
-    menu[:open_twitter] = {
-      :slug => :open_twitter,
-      :name => 'この人のtwitterを見る', 
-      :show_face => lambda{ |m|
-          u = if(m.message[:retweet])
-                m.message[:retweet].user
+Plugin.create :open_twitter do
+    command(:open_twitter,
+      name: 'この人のtwitterを見る',
+      condition: Proc.new{ |m|
+          u = if(m.messages.first.message[:retweet])
+                m.messages.first.message[:retweet].user
               else
-                m.message.user end
-          "#{u[:idname]}さんのtwitter Webへ".gsub(/_/, '__') },
-      :condition => lambda{ |m| m.message.repliable? },
-      :exec => lambda{ |m| Gtk::openurl("http://twitter.com/#!/#{m.message.user.idname}") },
-      :visible => true,
-      :role => :message }
-    [menu]
-  }
+                m.messages.first.message.user end
+          "#{u[:idname]}さんのtwitterへ".gsub(/_/, '__') },
+      visible: true,
+      role: :timeline) do |opt|
+        Gtk::openurl("http://twitter.com/#{opt.messages.first.message.idname}")
+  end
 end
