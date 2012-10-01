9673d007e93c9a186fcc802d7f59e55914230142
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2012-10-02 00:58:20 +0900

    show_faceとconditionを混同していたのを修正

--- open_twitter.rb
+++ open_twitter.rb
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
 # twitter Webを開く
 
-Plugin.create :open_twitter do
-    command(:open_twitter,
-      name: 'この人のtwitterを見る',
-      condition: Proc.new{ |m|
-          u = if(m.messages.first.message[:retweet])
-                m.messages.first.message[:retweet].user
+Module.new do
+  Plugin.create(:open_twitter).add_event_filter(:command){ |menu|
+    menu[:open_twitter] = {
+      :slug => :open_twitter,
+      :name => 'この人のtwitterを見る', 
+      :show_face => lambda{ |m|
+          u = if(m.message[:retweet])
+                m.message[:retweet].user
               else
-                m.messages.first.message.user end
-          "#{u[:idname]}さんのtwitterへ".gsub(/_/, '__') },
-      visible: true,
-      role: :timeline) do |opt|
-        Gtk::openurl("http://twitter.com/#{opt.messages.first.message.idname}")
-  end
+                m.message.user end
+          "#{u[:idname]}さんのtwitter Webへ".gsub(/_/, '__') },
+      :condition => lambda{ |m| m.message.repliable? },
+      :exec => lambda{ |m| Gtk::openurl("http://twitter.com/#!/#{m.message.user.idname}") },
+      :visible => true,
+      :role => :message }
+    [menu]
+  }
 end
